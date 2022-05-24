ae4072622f0f6123d231073c3da2dd29a42f4c3f
Rajesh Subedi <subedi.rajesh143@gmail.com>
2022-05-24 16:15:37 +0545

    first commit

---
+++ HorizonSoftware/HorizonSoftware/HomePageDetail.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HorizonSoftware
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePageDetail : ContentPage
+    {
+
+
+        SqlConnection sqlConnection;
+        public HomePageDetail()
+        {
+            var navigationPage = Application.Current.MainPage as NavigationPage;
+            navigationPage.BarBackgroundColor = Color.Black;
+            InitializeComponent();
+            string srvrdbname = "mydb";
+            string srvrname = "192.168.1.96";
+            string srvrusername = "Rajesh";
+            string srvrpassword = "12345";
+            string sqlconn = $"Data Source={srvrname};Initial Catalog={srvrdbname};User ID={srvrusername};Password={srvrpassword}";
+            sqlConnection = new SqlConnection(sqlconn);
+        }
+
+        private async void Close_Clicked_1(object sender, EventArgs e)
+        {
+            var result = await this.DisplayAlert("Alert!", "Do you want to Logout?", "yes", "No");
+            if (result == true)
+            {
+                _ = Navigation.PushAsync(new LoginPage());
+            }
+            else
+            {
+                //_ = Navigation.PushAsync(new HomePageDetail());
+            }
+        }
+
+
+
+
+        protected override bool OnBackButtonPressed()
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Alert!", "Do you really want to exit?", "Yes", "No");
+
+                if (result == true)
+                {
+                    /*    System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();*/ // Or anything else
+                    _ = Navigation.PushAsync(new LoginPage());
+                }
+            });
+            return true;
+        }
+
+        private void Ordertable_Clicked(object sender, EventArgs e)
+        {
+            _ = Navigation.PushAsync(new OrderTable());          
+        }
+
+
+
+
+        private void Amountpreview_Clicked(object sender, EventArgs e)
+        {
+            _ = Navigation.PushAsync(new AmountPreview());
+        }
+    }
+}
---
+++ HorizonSoftware/HorizonSoftware/OrderTable.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HorizonSoftware
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OrderTable : ContentPage
+    {
+        public class mysqlList
+        {
+            public string URL { get; set; }
+            public string TableName { get; set; }
+        }
+
+       
+
+
+        public ObservableCollection<string> mysqlLists { get; set; }
+        public string TableName { get; private set; }
+
+        ObservableCollection<string> data = new ObservableCollection<string>();
+
+        SqlConnection sqlConnection;
+        public OrderTable()
+        {
+            InitializeComponent();
+            string srvrdbname = "mydb";
+            string srvrname = "192.168.1.96";
+            string srvrusername = "Rajesh";
+            string srvrpassword = "12345";
+            string sqlconn = $"Data Source={srvrname};Initial Catalog={srvrdbname};User ID={srvrusername};Password={srvrpassword}";
+            sqlConnection = new SqlConnection(sqlconn);
+
+            List<mysqlList> mysqlLists = new List<mysqlList>();
+            sqlConnection.Open();
+            SqlCommand command = new SqlCommand("select * from dbo.Tabledb", sqlConnection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                mysqlLists.Add(new mysqlList
+                {
+                    URL = reader["URL"].ToString(),
+                    TableName = reader["TableName"].ToString(),
+
+                }
+                  );
+    
+            }
+            reader.Close();
+            //reader.Dispose();
+            sqlConnection.Close();
+            MyListView.ItemsSource = mysqlLists;
+        }
+
+
+
+        private async void SearchTable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+
+                List<mysqlList> mysqlLists = new List<mysqlList>();
+                sqlConnection.Open();
+                string queryString = $"select URL,TableName from dbo.Tabledb Where TableName like '%{e.NewTextValue}%'";
+
+                SqlCommand command = new SqlCommand(queryString, sqlConnection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    mysqlLists.Add(new mysqlList
+                    {
+                        URL = reader["URL"].ToString(),
+                        TableName = reader["TableName"].ToString(),
+                    }
+                    );
+                }
+                reader.Close();
+                sqlConnection.Close();
+                MyListView.ItemsSource = mysqlLists;
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", ex.Message, "Ok");
+                throw;
+            }
+        }
+
+
+
+
+    
+
+        private void MyListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+            {
+                return;
+            }
+
+            var selectedItem = (mysqlList)e.Item; // model
+            Navigation.PushAsync(new ItemsView(selectedItem.TableName,selectedItem.URL)); // pass the selected whole item from list to DetaiPage 'selectedItem' using constructor
+            ((ListView)sender).SelectedItem = null;
+
+     
+        }
+
+
+    }
+}
